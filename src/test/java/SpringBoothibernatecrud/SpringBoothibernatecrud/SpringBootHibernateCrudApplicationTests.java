653452a61fead2734b6407543a59ba1886c582c5
Lyubomyr <bobboby403@gmail.com>
2023-02-19 22:32:04 +0200

    start project version 1.0

---
+++ src/test/java/SpringBoothibernatecrud/SpringBoothibernatecrud/SpringBootHibernateCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoothibernatecrud.SpringBoothibernatecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
